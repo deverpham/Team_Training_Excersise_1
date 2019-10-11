a966f429ea66f6d299f2c1ee54f798f399c4562a
HoangNam <phamhoangnam55@gmail.com>
2019-10-11 20:00:54 +0700

    version3

--- src/operation/operation.js
+++ src/operation/operation.js
@@ -1,16 +1,16 @@
-var x = document.getElementById("libraries")
-var y = document.getElementById("libraries__box")
-var z = document.getElementsByClassName("boxMenu_newStyle")
+var getLibraries = document.getElementById("libraries")
+var getLibrariesBox = document.getElementById("libraries__box")
+var getBoxMeNuStyle = document.getElementsByClassName("boxMenu_newStyle")
 x.addEventListener("click", function(){
    
-    if(y.style.display == 'none'){
+    if(getLibrariesBox.style.display == 'none'){
         //y.classList.add("z")
-        y.style.marginTop = "5em"
-        y.style.display = "flex"
-        y.style.flexWrap = "wrap"
-        y.style.position = "absolute"
-        y.style.zIndex = "3"
+        getLibrariesBox.style.marginTop = "5em"
+        getLibrariesBox.style.display = "flex"
+        getLibrariesBox.style.flexWrap = "wrap"
+        getLibrariesBox.style.position = "absolute"
+        getLibrariesBox.style.zIndex = "3"
     }
     else
-        y.style.display = "none"
+        getLibrariesBox.style.display = "none"
 });
