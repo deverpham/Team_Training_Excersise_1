1d1bf5844a2209f6cdc2a4424c2ba80d355f9e56
HoangNam <phamhoangnam55@gmail.com>
2019-10-14 19:13:44 +0700

    operation click js

--- src/operation/operation.js
+++ src/operation/operation.js
@@ -1,16 +1,33 @@
 var getLibraries = document.getElementById("libraries")
 var getLibrariesBox = document.getElementById("libraries__box")
 var getBoxMenuStyle = document.getElementsByClassName("boxMenu_newStyle")
+var getNavigation = document.getElementById("navigation")
+var getLi = document.getElementById("id--li")
+var getLiByTag = document.getElementsByTagName("li")
+var getAByTag = document.getElementsByClassName("a")
+
 getLibraries.addEventListener("click", function(){
-   
-    if(getLibrariesBox.style.display == 'none'){
-        //y.classList.add("z")
-        getLibrariesBox.style.marginTop = "5em"
-        getLibrariesBox.style.display = "flex"
-        getLibrariesBox.style.flexWrap = "wrap"
-        getLibrariesBox.style.position = "absolute"
-        getLibrariesBox.style.zIndex = "3"
+
+    getLi.style.display = "flex"
+    
+    for(var i = 0 ;i < getAByTag.length; i++) {
+        getAByTag[i].style.display = "flex";
+        console.log(getAByTag[i])
     }
-    else
-        getLibrariesBox.style.display = "none"
+   
+   
+    // if(getLibrariesBox.style.display == 'none'){
+    //     //y.classList.add("z")
+    //     getLibrariesBox.style.marginTop = "5em"
+    //     getLibrariesBox.style.display = "flex"
+    //     getLibrariesBox.style.flexWrap = "wrap"
+    //     getLibrariesBox.style.position = "absolute"
+    //     getLibrariesBox.style.zIndex = "3"
+    // }
+    // else
+    //     getLibrariesBox.style.display = "none"
 });
+
+
+
+
