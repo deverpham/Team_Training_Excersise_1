c44002d9f0d04def8ad3937289ed88732ed84726
HoangNam <phamhoangnam55@gmail.com>
2019-10-15 16:19:47 +0700

    fuction dropdown nav

--- src/operation/operation.js
+++ src/operation/operation.js
@@ -1,32 +1,60 @@
-var getLibraries = document.getElementById("libraries")
-var getLibrariesBox = document.getElementById("libraries__box")
-var getBoxMenuStyle = document.getElementsByClassName("boxMenu_newStyle")
-var getNavigation = document.getElementById("navigation")
-var getLi = document.getElementById("id--li")
-var getLiByTag = document.getElementsByTagName("li")
-var getAByTag = document.getElementsByClassName("a")
-
-getLibraries.addEventListener("click", function(){
-
-    getLi.style.display = "flex"
-    
-    for(var i = 0 ;i < getAByTag.length; i++) {
-        getAByTag[i].style.display = "flex";
-        console.log(getAByTag[i])
+var libraries = document.getElementById("libraries")
+var librariesBox = document.getElementById("libraries__box")
+var boxMenuStyle = document.getElementsByClassName("boxMenu_newStyle")
+var li = document.getElementsByClassName("class--li")
+var liByTag = document.getElementsByTagName("li")
+var aByTag = document.getElementsByClassName("navi__a")
+var navigation = document.getElementById("navigation")
+var nav_libraries = document.getElementById("library")
+console.dir(nav_libraries)
+libraries.addEventListener("click", function() {  
+    navigation.classList.toggle("navigation__mobile")
+    for(var i = 0; i < li.length; i++) {
+        aByTag[i].classList.toggle("a--show")
+        li[i].classList.toggle("li--show")
     }
-   
-   
-    // if(getLibrariesBox.style.display == 'none'){
-    //     //y.classList.add("z")
-    //     getLibrariesBox.style.marginTop = "5em"
-    //     getLibrariesBox.style.display = "flex"
-    //     getLibrariesBox.style.flexWrap = "wrap"
-    //     getLibrariesBox.style.position = "absolute"
-    //     getLibrariesBox.style.zIndex = "3"
+    // if(getLi[0].style.display == 'none')
+    // {
+    //     navigation.classList.add = "boxMenu_newStyle "
+    //     navigation.style.display = "flex"
+    //     navigation.style.flexDirection = "column";
+    //     navigation.style.position = "absolute"
+    //     for(var i = 0 ;i < getAByTag.length; i++) {
+    //         getLi[i].style.display = "flex";
+    //         getAByTag[i].style.display = "flex";
+    //         console.log(getAByTag[i])
+    //     }
     // }
-    // else
-    //     getLibrariesBox.style.display = "none"
+    // else {
+    //     for(var i = 0 ;i < getAByTag.length; i++) {
+    //         getLi[i].style.display = "none";
+    //         getAByTag[i].style.display = "none";
+    //         console.log(getAByTag[i])
+    //     }
+    // }
+   
+       
+        // getLibrariesBox.style.marginTop = "5em"
+        // getLibrariesBox.style.display = "flex"
+        // getLibrariesBox.style.flexWrap = "wrap"
+        // getLibrariesBox.style.position = "absolute"
+        // getLibrariesBox.style.zIndex = "3"
 });
+
+nav_libraries.addEventListener("mouseover", function() {   
+    librariesBox.classList.toggle("boxMenu_newStyle")
+});
+
+nav_libraries.addEventListener("mousedown", function() {   
+    librariesBox.classList.toggle("boxMenu_newStyle")
+});
+
+nav_libraries.addEventListener("mouseleave", function() {
+  
+    librariesBox.classList.toggle("boxMenu_newStyle")
+});
+
+
 
 
 
