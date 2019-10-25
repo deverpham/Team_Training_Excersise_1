acf4d65e354e02bb2270462c573df292c2be8947
HoangNam <phamhoangnam55@gmail.com>
2019-10-25 19:11:17 +0700

    login

--- src/operation/operation.js
+++ src/operation/operation.js
@@ -1,61 +1,98 @@
-var libraries = document.getElementById("libraries")
-var librariesBox = document.getElementById("libraries__box")
-var boxMenuStyle = document.getElementsByClassName("boxMenu_newStyle")
-var li = document.getElementsByClassName("class--li")
-var liByTag = document.getElementsByTagName("li")
-var aByTag = document.getElementsByClassName("navi__a")
-var navigation = document.getElementById("navigation")
-var nav_libraries = document.getElementById("library")
-console.dir(nav_libraries)
-libraries.addEventListener("click", function() {  
-    navigation.classList.toggle("navigation__mobile")
-    for(var i = 0; i < li.length; i++) {
-        aByTag[i].classList.toggle("a--show")
-        li[i].classList.toggle("li--show")
-    }
-    // if(getLi[0].style.display == 'none')
-    // {
-    //     navigation.classList.add = "boxMenu_newStyle "
-    //     navigation.style.display = "flex"
-    //     navigation.style.flexDirection = "column";
-    //     navigation.style.position = "absolute"
-    //     for(var i = 0 ;i < getAByTag.length; i++) {
-    //         getLi[i].style.display = "flex";
-    //         getAByTag[i].style.display = "flex";
-    //         console.log(getAByTag[i])
-    //     }
-    // }
-    // else {
-    //     for(var i = 0 ;i < getAByTag.length; i++) {
-    //         getLi[i].style.display = "none";
-    //         getAByTag[i].style.display = "none";
-    //         console.log(getAByTag[i])
-    //     }
-    // }
+
+// function about nav
+function navigationText() {
+    var libraries = document.getElementById("libraries")
+    var librariesBox = document.getElementById("libraries__box")
+    var boxMenuStyle = document.getElementsByClassName("boxMenu_newStyle")
+    var li = document.getElementsByClassName("class--li")
+    var liByTag = document.getElementsByTagName("li")
+    var aByTag = document.getElementsByClassName("navi__a")
+    var navigation = document.getElementById("navigation")
+    var nav_libraries = document.getElementById("library")
+    libraries.addEventListener("click", function() {  
+        navigation.classList.toggle("navigation__mobile")
+        for(var i = 0; i < li.length; i++) {
+            aByTag[i].classList.toggle("a--show")
+            li[i].classList.toggle("li--show")
+        }
+    });
+
+    nav_libraries.addEventListener("mouseover", function() {   
+        librariesBox.classList.toggle("boxMenu_newStyle")
+    });
+    
+    nav_libraries.addEventListener("mousedown", function() {   
+        librariesBox.classList.toggle("boxMenu_newStyle")
+    });
+    
+    nav_libraries.addEventListener("mouseleave", function() {
+      
+        librariesBox.classList.toggle("boxMenu_newStyle")
+    });    
+}
+
+//function about email input
+
+var messageButton = document.getElementById("mess__btn")
+function emailWarning() {
+    var email = document.getElementsByClassName("email")
+    var warn__text = document.getElementById("warn__Etext")
+    var str = "The email must be a email and the input cannot be empty"
+    email[0].addEventListener("blur", function(){
+        if(this.value.indexOf("@email")!=-1 || 
+        this.value.indexOf("@gmail")!=-1
+        || this.value.indexOf("@yahoo")!=-1) {
+            this.style.borderColor = "white"
+            flat = 0
+            warn__text.innerHTML = ""
+        }
+        else {
+            this.style.borderColor = "red"
+            warn__text.innerHTML = str
+        }
+    })
+}
+
+//function about text input
+function textWarning() {
+    var warn__Stext = document.getElementById("warn__Stext")
+    var textBox = document.getElementsByTagName("input")
+    var str = "The input cannot be empty"
+    for(var i = 1; i<textBox.length; i++) {
+            textBox[i].addEventListener("blur", function () {
+                if(this.value == ""){
+                    this.style.borderColor = "red";
+                    warn__Stext.innerHTML = str
+                }
+                else {
+                    this.style.borderColor = "white"
+                    warn__Stext.innerHTML = ""
+                }
+            }
+        )}
+}
+
+
+navigationText();
+emailWarning();
+textWarning();
+
+
+//     var i = 0
+//      while(i < textBox.length) {
+//     //  textBox[i].style.borderColor = "red" 
+//     textBox[i].addEventListener("blur", function() {
+//         if(textBox[i].value == ""){
+//             textBox[i].style.borderColor = "red"
+//         }
+//         else {
+//             textBox[i].style.borderColor = "white"
+//         }
+//     })
+//     i++;
+// }
+
    
-       
-        // getLibrariesBox.style.marginTop = "5em"
-        // getLibrariesBox.style.display = "flex"
-        // getLibrariesBox.style.flexWrap = "wrap"
-        // getLibrariesBox.style.position = "absolute"
-        // getLibrariesBox.style.zIndex = "3"
-});
-
-nav_libraries.addEventListener("mouseover", function() {   
-    librariesBox.classList.toggle("boxMenu_newStyle")
-});
-
-nav_libraries.addEventListener("mousedown", function() {   
-    librariesBox.classList.toggle("boxMenu_newStyle")
-});
-
-nav_libraries.addEventListener("mouseleave", function() {
-  
-    librariesBox.classList.toggle("boxMenu_newStyle")
-});
-
-
-//input
 
 
 
